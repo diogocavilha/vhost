88cfcf8bd30ee55f6ba6cc78b541fc1a1712ef1a
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2015-09-09 21:24:04 -0300

    Check directories before checking if file exists.
    Now it checks all possible directories which the file might
    be in. This is done in order to verify whether the file exists.

--- vhost.sh
+++ vhost.sh
@@ -237,6 +237,8 @@ appendEnvironmentVars()
 #==================================================================#
 requireFileName()
 {
+    local file
+
     echo -n -e "\n Nome do arquivo: "
     read vhFileName
 
@@ -246,9 +248,15 @@ requireFileName()
             vhFileName=${vhFileName}.conf
         fi
 
-        $(fileExists "$SITES_AVAILABLE/$vhFileName")
+        file=$SITES_DIRECTLY/$vhFileName
+
+        if [ "$SITES_DIRECTLY" = "" ]; then
+            file=$SITES_AVAILABLE/$vhFileName
+        fi
+
+        $(fileExists "$file")
         if [ $? -eq 1 ]; then
-            messageRed "\n [$SITES_AVAILABLE/$vhFileName] Arquivo já existe.\n"
+            messageRed "\n [$file] Arquivo já existe.\n"
             requireFileName
         fi
     else
